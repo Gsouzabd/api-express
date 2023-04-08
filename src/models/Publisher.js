d33076d6a79d60334eed15d4ebf3ba909ce47660
gsouzabd <gabrielsouzabd@outlook.com>
2023-04-08 17:42:43 -0300

    main

---
+++ src/models/Publisher.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const publisherSchema = new mongoose.Schema(
+    {
+        id: {type: String},
+        name: {type: String, required: true},
+        country: {type: String}
+
+    }
+)
+
+const publishers = mongoose.model('publishers ', publisherSchema)
+
+export default publishers;
