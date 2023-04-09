a33a41d969b0b4a6b37f8468eb0d1155e9a4f23a
gsouzabd <gabrielsouzabd@outlook.com>
2023-04-09 15:16:13 -0300

    .env and async functions

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -2,138 +2,164 @@ import books from "../models/Book.js";
 
 class BooksController{
 
-    /*
+  /*
     ** List all books from database
     */
-    static getAllBooks = (req,res) =>{
+  static getAllBooks = async (req,res) =>{
 
-        books.find()
-        .populate('author')
-        .populate('publisher')
-        .exec((error,books)=>{
-            res.status(200).json(books);
-    
-        });
-        
-    }
+    try {
+      const book = await books.find()
+        .populate("author")
+        .populate("publisher")
+        .exec();
+
+      if(book !== null) {
+        res.status(200).json(book);
+      }
+    } catch (error) {
+      res.status(500).json({message: `${error.message} `});
+    }     
+  };
 
 
-    /*
+  /*
     ** Retrieve a book from database
     */
-    static retrieveBook = (req,res) =>{
+  static retrieveBook = async (req,res) =>{
 
-        const {id} = req.params;
+    try {
+      const {id} = req.params;
 
-        books.findById(id)
-        .populate('author')
-        .populate('publisher')
-        .exec((error, books)=>{
-            if(error){
-                res.status(400).send({message: `${error.message} - Book not found`})
-            }else{
-                res.status(200).json(books)
-            }
-        })
+      const book = await books.findById(id)
+        .populate("author")
+        .populate("publisher")
+        .exec();
+
+
+      if (book !== null) {
+        res.status(201).send(book);
+
+      }
+
+      
+    } catch (error) {
+      res.status(404).json({message: "Not Found"});
     }
+  };
 
 
-    /*
+  /*
     ** Search a book with conditions passed in query 
     */
-    static findByParams = (req, res) =>{
+  static findByParams = async (req, res) =>{
 
-        const publisher = req.query.publisher;
-        const author = req.query.author;
+    const publisher = req.query.publisher;
+    const author = req.query.author;
+  
+    if(publisher && author){
+      try {
+        const book = await books.find({"author": author, "publisher": publisher});
 
-        if(publisher && author){
-            books.find({'author': author, 'publisher': publisher}, {}, (error, book)=>{
-                    res.status(200).send(book);
-
-            })
+        if (book !== null) {
+          res.status(200).send(book);
         }
+  
+      }catch (error) {
+        res.status(404).json({message: "Book not found"});
+      }
+    }
+  
+    else if(author){
+      try{
+        const book = await books.find({"author": author});
 
-        else if(author){
-            books.find({'author': author}, {}, (error, book) =>{
-                if(!error){
-                    res.status(200).send(book);
-                }else{
-                    res.status(400).send({message: `${error.message} - Book not found by author`})
-                }
-            })
+        if (book !== null) {
+          res.status(200).send(book);
         }
+      }catch(error){
+        res.status(404).send({message: `${error.message} - Book not found by author`});
 
-        else if(publisher){
-            books.find({'publisher': publisher}, {}, (error, book) =>{
-                if(!error){
-                    res.status(200).send(book);
-                }else{
-                    res.status(400).send({message: `${error.message} - Book not found by publisher`})
-                }
-            })
+      }
+    }
+  
+    else if(publisher){
+      try {
+        const book = await books.find({"publisher": publisher});
+          
+        if (book !== null) {
+          res.status(200).send(book);
         }
+          
+      } catch (error) {
+        res.status(404).send({message: `${error.message} - Book not found by publisher`});
 
-
-
-
+      }
     }
 
+  }; 
 
-    /*
+
+  /*
     ** Insert a book in database
     */
-    static createBook = (req,res) =>{
+  static createBook = async (req,res) =>{
 
-        const book = new books(req.body);
+    try {
+      const book = await new books(req.body);
 
-        book.save(((error)=>{
-            if(error){
-                res.status(500).send({message: `${error.message} - fail trying to create book`})
-            }else{
-                res.status(201).send(JSON.stringify(book));
-            }
-        }))
+      book.save();
 
+      if (book !== null) {
+        res.status(201).send(JSON.stringify(book));
+      }
+     
+    } catch (error) {
+      res.status(500).json({message: `${error.message}`});
     }
 
-    /*
+  };
+
+  /*
     ** Update a book by id
     */
-    static updateBook = (req,res) =>{
+  static updateBook = async (req,res) =>{
 
-        let {id} = req.params;
+    try {
 
-        books.findByIdAndUpdate(id, {$set: req.body}, (error) =>{
-            if(!error){
-                res.status(201).send({message:`Book successfully updated!`});
-            }else{
-                res.status(500).send({message: `${error.message} - fail trying to create book`})
-            }
-        })
-        
+      let {id} = req.params;
+
+      const book = await books.findByIdAndUpdate(id, {$set: req.body});
+      if (book !== null) {
+        res.status(201).send({message:"Book successfully updated!"});
+      }
+      
+    } catch (error) {
+      res.status(404).json({message: `Book not found. ${error.message}`});
     }
+        
+  };
 
 
-    /*
+  /*
     ** Delete a book from database
     */
-    static deleteBook = (req,res) =>{
+  static deleteBook = async (req,res) =>{
 
-        let {id} = req.params;
+    try {
+      let {id} = req.params;
 
-        books.findByIdAndDelete(id, (error) =>{
-            if(!error){
-                res.status(200).send({message: 'Book succesfully deleted'})
+      await books.findByIdAndDelete(id);
 
-            }else{
-                res.status(400).send({message: `${error.message} - Book not found`})
-            }
+      res.status(201).send({message:"Book successfully deleted!"});
 
-        })
+    } catch (error) {
+      res.status(404).json({message: `Book not found. ${error.message}`});
     }
+
+  };
 
 
 }
 
 
-export default BooksController
+export default BooksController;
