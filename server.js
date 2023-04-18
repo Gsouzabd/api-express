cc61d4842c4bed0ba61c030e226fcd808e221ce6
Gabriel Souza <80110926+Gsouzabd@users.noreply.github.com>
2023-04-18 19:10:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import app from "./src/app.js";
 
 const port= process.env.PORT || 3000;
 
-app.listen(port, ()=>{
+app.listen((process.env.PORT || 3000), ()=>{
   console.log(`Server listening to port: http://localhost:${port}`);
 });
